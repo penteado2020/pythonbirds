57b15a5f3a99c8b12fa3dfe18d9aa95dc7641450
penteado2020 <jjjpenteado@gmail.com>
2020-08-02 13:18:21 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,22 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        #Atributo nome é = self.nome
+        # e abaixo eu passo o valor do atributo nome o valor do parametro nome
+        # alt+Enter já cria a linha de inclusão do atributo, informaado no parametro acima,
+        # exemplo idade=35 criará a linha self.idade = idade
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__== '__main__':
-    p = Pessoa()
+    p = Pessoa("jeje")
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Jefferson'
+    print(p.nome)
+    print(p.idade)
