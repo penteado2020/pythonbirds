abbac64d02828cd1f03ad5ea7fd002345defbe79
penteado2020 <jjjpenteado@gmail.com>
2020-08-03 07:39:08 -0300

    Criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,22 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         #Atributo nome é = self.nome
         # e abaixo eu passo o valor do atributo nome o valor do parametro nome
         # alt+Enter já cria a linha de inclusão do atributo, informaado no parametro acima,
         # exemplo idade=35 criará a linha self.idade = idade
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__== '__main__':
-    p = Pessoa("jeje")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Jefferson'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome="Renzo")
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    #print(luciano.filhos)
